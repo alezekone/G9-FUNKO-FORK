f27d28276c90491896c5c558676d4e9846953fc7
alezekone <alezek@gmail.com>
2023-12-11 16:29:06 -0300

    Inserción dinámica en shop.ejs. Primera prueba.

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,9 +1,10 @@
 const testData = require('../model/datos');
 
-console.log(testData);
+// console.log(testData);
+// console.log(testData.at(1));
 
 const shopControllers = {
-    shop_get : (req, res) => res.render('shop/shop',{}),    // "Ruta para la vista de Shop."
+    shop_get : (req, res) => res.render('shop/shop',{data: testData}),
     shop_item_get : (req, res) => res.render('shop/item',{}),
     shop_item_post : (req, res) => res.send(`Ruta para agregar un item. Parámetro: ${req.params.id}`),
     shop_cart_get : (req, res) => res.render('shop/cart'),
